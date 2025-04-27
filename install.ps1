22efa0f0f4f96ae835a466fb8da2723d9d966144
Slava Zalygin <vya.zalygin@gmail.com>
2025-04-27 19:13:54 +0300

    Докинул скрипты установки (#2)
    * Скрипт + каркас гайда
    
    * add git existence check
    
    * fix
    
    * fix docs to local
    
    * add script for windows
    
    * add typst installing
    
    * fix install.sh
    
    * update readme
    
    * починил скрипт установки
    
    ---------
    
    Co-authored-by: v.zalygin <v.zalygin@tbank.ru>

---
+++ install.ps1
@@ -0,0 +1,92 @@
+#!/usr/bin/env pwsh
+# Keep this script simple and easily auditable!
+$ErrorActionPreference = "Stop"
+
+try {
+
+Push-Location
+
+if (-not (Get-Command git -ErrorAction SilentlyContinue)) {
+    Write-Host -ForegroundColor "red" "Script needs git installed to run"
+    exit 1
+}
+
+if ($args.Count -eq 2) {
+    $bmstu_tag = $args[0]
+    $gost_tag = $args[1]
+}
+elseif ($args.Count -eq 1) {
+    $bmstu_tag = $args[0]
+    $gost_tag = $args[0]
+}
+else {
+    $bmstu_tag = "latest"
+    $gost_tag = "latest"
+}
+
+Write-Host "Installing typst 0.12.0"
+
+$v="0.12.0"; irm https://typst.community/typst-install/install.ps1 | iex
+
+Write-Host "Installing 'bmstu' (version='$bmstu_tag') and 'gost7.32-2017' (version='$gost_tag') packages..."
+
+$home_dir = "$env:APPDATA\typst\packages\local"
+
+$clone_dir = [System.IO.Path]::Combine($env:TEMP, "typst")
+
+# Clean clone directory if exists
+if (Test-Path $clone_dir) {
+    Remove-Item -Recurse -Force $clone_dir
+}
+New-Item -ItemType Directory -Path $clone_dir
+
+# BMSTU
+git clone "https://github.com/bmstudents/typst-bmstu.git" "$clone_dir\bmstu"
+Set-Location "$clone_dir\bmstu"
+if ($bmstu_tag -eq "latest") {
+    $bmstu_tag = git describe --tags (git rev-list --tags --max-count=1)
+}
+git checkout -q $bmstu_tag
+
+$package_dir = [System.IO.Path]::Combine($home_dir, "bmstu\$bmstu_tag")
+if (Test-Path $package_dir) {
+    Remove-Item -Recurse -Force $package_dir
+}
+
+New-Item -ItemType Directory -Path $package_dir
+Move-Item "$clone_dir\bmstu\bmstu\*" $package_dir
+Write-Host "Package 'bmstu' version='$bmstu_tag' was installed"
+
+# GOST
+git clone "https://github.com/bmstudents/typst-g7.32-2017.git" "$clone_dir\typst-g7.32-2017"
+
+Set-Location "$clone_dir\typst-g7.32-2017"
+if ($gost_tag -eq "latest") {
+    $gost_tag = git describe --tags (git rev-list --tags --max-count=1)
+}
+git checkout -q $gost_tag
+
+$package_dir = [System.IO.Path]::Combine($home_dir, "gost732-2017\$gost_tag")
+if (Test-Path $package_dir) {
+    Remove-Item -Recurse -Force $package_dir
+}
+
+New-Item -ItemType Directory -Path $package_dir
+Move-Item "$clone_dir\typst-g7.32-2017\gost732-2017\*" $package_dir
+Write-Host "Package 'gost 7.32-2017' version='$gost_tag' was installed"
+
+Set-Location $env:TEMP
+Remove-Item -Recurse -Force $clone_dir
+
+}
+catch {
+
+Write-Host -ForegroundColor "red" "Unexpected error during installation:"
+Write-Output -ForegroundColor "red" $_
+
+}
+finally {
+
+Pop-Location
+
+}
